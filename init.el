4d57768c9cdfb18ce206cef398d4141c1e73f937
Osushi <tdu.future.i@gmail.com>
2016-05-19 18:22:51 +0900

    Update emacs settings

--- init.el
+++ init.el
@@ -46,6 +46,13 @@
                 )))
 ;; 選択したリージョンに上書き
 (delete-selection-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; マイススクロールでカーソル移動
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
+(global-set-key [mouse-4] (kbd "C-p"))
+(global-set-key [mouse-5] (kbd "C-n"))
 
 ;; Set Cask Path
 (require 'cask)
