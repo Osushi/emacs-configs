12da88563e0d04a1e16ecff9ca0c4ed35ecd8910
Osushi <tdu.future.i@gmail.com>
2017-08-21 23:45:02 +0900

    Setting goimports

--- init.el
+++ init.el
@@ -136,6 +136,7 @@
 (add-to-list 'exec-path (expand-file-name "~/.goenv/bin"))
 (require 'go-autocomplete)
 (require 'go-mode)
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "M-.") 'godef-jump)))
